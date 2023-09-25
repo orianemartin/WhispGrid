c1e0e486b826759e6c8ea05435908a06d6571fe7
Oriane Martin <106679464+orianemartin@users.noreply.github.com>
2023-09-25 16:51:58 +0200

    Diarization
    Added diarization (speaker recognition).
    Added choice of language

--- whispgrid.py
+++ whispgrid.py
@@ -2,13 +2,36 @@ import whisper_timestamped as whisper
 import tgt
 import json
 import tkinter as tk
-from tkinter import filedialog, StringVar, OptionMenu, messagebox, ttk
+from tkinter import filedialog, StringVar, OptionMenu, messagebox, ttk, Entry
 from tkinter.simpledialog import askstring
 import sv_ttk
 import os
-import threading
 
-predefined_models = ["tiny", "small", "base", "medium", "large", "bofenghuang/whisper-medium-french"]
+import threading
+import datetime
+import subprocess
+
+import torch
+import pyannote.audio
+
+from pyannote.audio import Audio
+from pyannote.core import Segment
+
+from pyannote.audio.pipelines.speaker_verification import PretrainedSpeakerEmbedding
+embedding_model = PretrainedSpeakerEmbedding(
+    "speechbrain/spkrec-ecapa-voxceleb",
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+
+import wave
+import contextlib
+
+from sklearn.cluster import AgglomerativeClustering
+import numpy as np
+
+predefined_models = ["small", "base", "medium", "large", "bofenghuang/whisper-medium-french"]
+predefined_languages = ["en", "fr", "es", "de"]
+num_speakers = 0 
+
 
 def select_audio_files():
     file_paths = filedialog.askopenfilenames(filetypes=[("Audio files", "*.wav;*.mp3;*.mp4;*.mpeg;*.mpga;*.m4a;*.webm;*.flac;*.ogg")])
@@ -18,12 +41,28 @@ def select_audio_files():
 def transcribe_audios():
     selected_files = audio_listbox.get(0, tk.END)
     selected_model = model_var.get()
+    selected_language = language_var.get()
+    num_speakers = speaker_entry.get()
+
+    # Check if num_speakers is empty or 0
+    if not num_speakers or int(num_speakers) == 0:
+        messagebox.showerror("Invalid Input", "Please enter a valid number of speakers.")
+        return
 
     if selected_model == "other":
         custom_model = askstring("Custom Model", "Enter a custom model name:")
         if custom_model is None:
             return
         selected_model = custom_model
+
+    num_speakers = int(num_speakers)
+
+    if selected_language == "other":
+        custom_language = askstring("Custom Language", "Enter the language code:")
+        if selected_language is None:
+            return
+        selected_language = custom_language
+
 
     transcribe_button.config(state=tk.DISABLED)
 
@@ -35,7 +74,7 @@ def transcribe_audios():
 
     transcription_threads = []
     for audio_path in selected_files:
-        transcription_thread = threading.Thread(target=transcribe_audio_thread, args=(audio_path, selected_model, on_transcription_completed, transcription_threads))
+        transcription_thread = threading.Thread(target=transcribe_audio_thread, args=(audio_path, selected_model, on_transcription_completed, transcription_threads, selected_language, int(num_speakers)))
         transcription_threads.append(transcription_thread)
         transcription_thread.start()
 
@@ -47,7 +86,14 @@ def transcribe_audios():
 
     check_transcription_completion()
 
-def transcribe_audio_thread(audio_path, selected_model, on_transcription_completed, transcription_threads):
+def transcribe_audio_thread(audio_path, selected_model, on_transcription_completed, transcription_threads, selected_language, num_speakers):
+    
+    original_file_name, original_file_ext = os.path.splitext(os.path.basename(audio_path))
+
+    if audio_path[-3:] != 'wav':
+        subprocess.call(['ffmpeg', '-i', audio_path, f'{original_file_name}.wav', '-y'])
+        audio_path = f'{original_file_name}.wav'
+
     audio = whisper.load_audio(audio_path)
 
     model = whisper.load_model(selected_model, device="cpu")
@@ -55,26 +101,67 @@ def transcribe_audio_thread(audio_path, selected_model, on_transcription_complet
     result = whisper.transcribe(
         model,
         audio,
-        language="fr",
+        language=selected_language,
         beam_size=5,
         best_of=5,
         temperature=(0.0, 0.2, 0.4, 0.6, 0.8, 1.0),
         trust_whisper_timestamps=False
     )
 
+    segments = result["segments"]
+    previous_end_time = 0.0
+
+
+#Create TextGrid and edit it
+
     tg = tgt.TextGrid()
 
     sentences_tier = tgt.IntervalTier(start_time=0, end_time=result["segments"][-1]["end"], name="phrase")
     word_tier = tgt.IntervalTier(start_time=0, end_time=result["segments"][-1]["end"], name="mot")
 
+    #work on words before it's too late
     for segment in result["segments"]:
-        interval = tgt.Interval(start_time=float(segment["start"]), end_time=float(segment["end"]), text=segment["text"])
-        sentences_tier.add_interval(interval)
+        if "words" in segment:
+            for word in segment["words"]:
+                interval = tgt.Interval(start_time=float(word["start"]), end_time=float(word["end"]), text=word["text"])
+                word_tier.add_interval(interval)
 
-    for segment in result["segments"]:
-        for word in segment["words"]:
-            interval = tgt.Interval(start_time=float(word["start"]), end_time=float(word["end"]), text=word["text"])
-            word_tier.add_interval(interval)
+    if int(num_speakers) > 1:
+
+        with contextlib.closing(wave.open(audio_path,'r')) as f:
+            frames = f.getnframes()
+            rate = f.getframerate()
+            duration = frames / float(rate)
+
+        audio_dia = Audio()
+
+        def segment_embedding(segment):
+            start = segment["start"]
+            # Whisper overshoots the end timestamp in the last segment
+            end = min(duration, segment["end"])
+            clip = Segment(start, end)
+            waveform, sample_rate = audio_dia.crop(audio_path, clip)
+            return embedding_model(waveform[None])
+
+        embeddings = np.zeros(shape=(len(segments), 192))
+        for i, segment in enumerate(segments):
+            embeddings[i] = segment_embedding(segment)
+
+        embeddings = np.nan_to_num(embeddings)
+
+        clustering = AgglomerativeClustering(num_speakers).fit(embeddings)
+        labels = clustering.labels_
+        for i in range(len(segments)):
+            segments[i]["speaker"] = str(labels[i] + 1)
+            concatenated_text = f"{segments[i]['speaker']} {segments[i]['text']}"
+            interval = tgt.Interval(start_time=float(segments[i]["start"]), end_time=float(segments[i]["end"]), text=concatenated_text)
+            sentences_tier.add_interval(interval)
+
+    else:
+        for segment in result["segments"]:
+            interval = tgt.Interval(start_time=float(segment["start"]), end_time=float(segment["end"]), text=segment["text"])
+            sentences_tier.add_interval(interval)
+
 
     tg.add_tier(sentences_tier)
     tg.add_tier(word_tier)
@@ -87,6 +174,8 @@ def transcribe_audio_thread(audio_path, selected_model, on_transcription_complet
 
     if all(not thread.is_alive() for thread in transcription_threads):
         on_transcription_completed()
+
+# App making
 
 app = tk.Tk()
 
@@ -103,10 +192,29 @@ audio_listbox.pack()
 model_label = ttk.Label(app, text="Select or Enter a Model:")
 model_label.pack()
 
+
 model_var = StringVar(app)
 model_var.set(predefined_models[0])
 model_option_menu = OptionMenu(app, model_var, *predefined_models, "other")
 model_option_menu.pack()
+
+#language to use
+
+language_label = ttk.Label(app, text="Select or Enter a Language:")
+language_label.pack()
+
+language_var = StringVar(app)
+language_var.set(predefined_languages[0])
+language_option_menu = OptionMenu(app, language_var, *predefined_languages, "other")
+language_option_menu.pack()
+
+#number of speakers
+
+speaker_label = ttk.Label(app, text="Enter the Number of Speakers:")
+speaker_label.pack()
+
+speaker_entry = Entry(app)
+speaker_entry.pack()
 
 select_button = ttk.Button(app, text="Select Audio Files", command=select_audio_files)
 select_button.pack()
